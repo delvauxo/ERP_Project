3f881532dbefd8a2407a1f2e7422891bdce5c4b3
delvauxo <delvauxo@outlook.com>
2022-02-03 22:51:30 +0100

    includes working clean

--- public/js/includes.js
+++ public/js/includes.js
@@ -1,38 +1,10 @@
-function includeHTML() {
-  return new Promise((resolve, reject) => {
-
-      let arrayIncludeNode = document.querySelectorAll("*[w3-include-html]")
-      let countNode = arrayIncludeNode.length
-
-      arrayIncludeNode.forEach((item, index) => {
-        let link = item.getAttribute("w3-include-html")
-
-        fetch(link, {
-          method: "GET"
-        })
-        .then((response) => {
-          if(response.ok)
-          {
-            response.text()
-            .then((datas) =>
-            {
-              console.log(datas.substring(0,10))
-              item.innerHTML = datas
-              item.removeAttribute("w3-include-html")
-
-              //resolve ici  --> ok ok c'est dégueu LOL
-              if(index == countNode-1)
-                resolve()
-            })
-          }
-        })
-        .catch((e) => {
-          item.innerHTML = "Page not found."
-        })
-      })
-
-  })
-  
+const includeHTMLs = async function(arrayIncludesWrappers) {
+  for (const include of arrayIncludesWrappers) {
+      const page = include.getAttribute('include-html')
+      const response = await fetch('./' + page + '.html', {method: 'GET'})
+      const text = await response.text()
+      include.innerHTML = text
+  }
 }
 
-export default includeHTML
+export default includeHTMLs
--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,15 @@
 import Supplier from "../models/Supplier.js"
 import Product from "../models/Product.js"
 import Customer from "../models/Customer.js"
-import includeHTML from "./includes.js"
+import includeHTMLs from "./includes.js"
 
+// Make HTML includes.
+await includeHTMLs(document.querySelectorAll('[include-html]'))
 
+// Create links listings.
+createListingDatas(document.querySelectorAll('[data-listing]'), document.querySelector('#btn-add'))
 
-// // Get datas from API.
-// const customers = await getCustomers()
-// const products = await getProducts()
-// const suppliers = await getSuppliers()
-
-
-includeHTML().then(() => {
-    // Create listing datas from API.
-    createListingDatas(document.querySelectorAll('#menu a[data-listing]'), document.querySelector('#btn-add'))
-    // On form Submits new form datas are sent to API database.
-    console.log(document.querySelector('#form-product'))
-    console.log(document.querySelector('#form-supplier'))
-    console.log(document.querySelector('#form-customer'))
-
-    formSubmit(document.querySelector('#form-product'), Product, insertProduct, getProducts, '#productModal')
-    formSubmit(document.querySelector('#form-supplier'), Supplier, insertSupplier, getSuppliers, '#supplierModal')
-    formSubmit(document.querySelector('#form-customer'), Customer, insertCustomer, getCustomers, '#customerModal')
-})
-
-
+// On form Submits new form datas are sent to API database.
+formSubmit(document.querySelector('#form-product'), Product, insertProduct, getProducts, '#productModal')
+formSubmit(document.querySelector('#form-supplier'), Supplier, insertSupplier, getSuppliers, '#supplierModal')
+formSubmit(document.querySelector('#form-customer'), Customer, insertCustomer, getCustomers, '#customerModal')
