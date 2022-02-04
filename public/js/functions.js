d7fdf6e6af0a31da28f416e73cd86a78765a9c5e
delvauxo <delvauxo@outlook.com>
2022-02-04 02:01:24 +0100

    sticky navbar

--- public/js/functions.js
+++ public/js/functions.js
@@ -280,3 +280,19 @@ function createTable(datas, htmlElem) {
         htmlElem.innerHTML = html
     }
 }
+
+// Navigation bar Menu fixed to top of the screen when scrolling down.
+document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 0) {
+          document.getElementById('navbar_top').classList.add('fixed-top');
+          // add padding top to show content behind navbar
+          navbar_height = document.querySelector('.navbar').offsetHeight;
+          document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+          document.getElementById('navbar_top').classList.remove('fixed-top');
+           // remove padding top from body
+          document.body.style.paddingTop = '0';
+        } 
+    });
+  }); 
